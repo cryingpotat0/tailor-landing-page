4df5afebe8d3f4a5266c2c45a755ec36dd10f8ed
Raghav <dev@raghav98.com>
2024-04-10 00:40:38 -0400

    setup posthog

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://tailor.cryingpotato.com",
   integrations: [tailwind(), react()]
 });
